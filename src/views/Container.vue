68b7f8ec8fe12fd0cd496409e26cb872b57cfaef
minwoo <mwoodev526@gmail.com>
2020-02-23 01:29:11 +0900

    [create] Create views file

---
+++ src/views/Container.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="container">
+  <vue-progress-bar></vue-progress-bar>
+  <transition name="routing-fade" mode="out-in">
+    <router-view @on:progress="onProgress" @off:progress="offProgress" ></router-view>
+  </transition>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "Container",
+    data() {
+      return {
+        loading: false
+      }
+    },
+    methods: {
+      onProgress() {
+        this.$Progress.start()
+      },
+      offProgress() {
+        this.$Progress.finish()
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/container';
+/* Router Transition */
+.routing-fade-enter-active, .routing-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.routing-fade-enter, .routing-fade-leave-to
+  /* .routing-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
---
+++ src/views/Navigation.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="nav">
+    <div class="logo_img">
+      <img src="https://songsin-resource.s3.ap-northeast-2.amazonaws.com/image/common/school_log.png">
+    </div>
+    <router-view name="nav"></router-view>
+
+  </div>
+</template>
+
+<script>
+  import MainNav from '@/components/nav/MainNav'
+  export default {
+    name: "Navigation",
+    components: {
+      MainNav
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/navigation.scss';
+.logo_img{
+  width: 5.5rem;
+  height: 6rem;
+  margin: 0 auto;
+  img{
+    width: 100%;
+    height: 100%;
+  }
+}
+.student_logo_img{
+  position: absolute;
+  bottom: 0;
+  img{
+    width: 100%
+  }
+}
+</style>
